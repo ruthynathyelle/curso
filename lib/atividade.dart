e78a35c2caf0e538c371a8855f5352d1e269b5f0
Ruthy Nathyelle <ruthy2015buritis@hotmail.com>
2024-09-17 18:11:08 -0300

    salvando

---
+++ lib/atividade.dart
@@ -0,0 +1,96 @@
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 100,
+                  height: 100,
+                ),
+                 Container(
+                  color: Colors.amber,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.black87,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.brown,
+                  width: 100,
+                  height: 100,
+                ),
+                 Container(
+                  color: Colors.pink[100],
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+               children: [
+                Container(
+                  color: Colors.pink,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: 100,
+                  height: 100,
+                ),
+                 Container(
+                  color: Colors.teal,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+              
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+*/
